323af7a413704fd1ddc13062870e5a768cbe2af8
shbodake <sayalibodake28@gmail.com>
2021-12-14 22:47:45 +0530

    Added: First Servlet

--- src/main/java/com/myfirsteservlet/FirstServlet.java
+++ src/main/java/com/myfirsteservlet/FirstServlet.java
@@ -10,7 +10,8 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 @WebServlet("/FirstServlet")
-public class FirstServlet extends HttpServlet {
+public class FirstServlet extends HttpServlet
+{
     @Override
     protected void doGet (HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html");
